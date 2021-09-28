cccfebb362d532f2c6219ab525106720658ec0cb
JLuisCuamatzi <jcuamatzi@liigh.unam.mx>
2021-09-27 20:23:49 -0500

    Script to extract the coverage by gene in a chromosome in each sample

--- 06_Extract_USMA_genes_coordinates.py
+++ 06_Extract_USMA_genes_coordinates.py
@@ -11,7 +11,7 @@
 ### Output:         SGE to do mapping and variant calling for several samples
 ###
 # How execute this script:
-#python3 /mnt/Timina/lmorales/Public/Ustilago/C1/bin/scripts/06_Extract_USMA_genes_coordinates.py -d /mnt/Timina/lmorales/Public/Ustilago/C1/ -f /mnt/Timina/lmorales/Public/Ustilago/C1/ID.csv
+#python3 /mnt/Timina/lmorales/Public/Ustilago/C1/bin/scripts/06_Extract_USMA_genes_coordinates.py -d /mnt/Timina/lmorales/Public/Ustilago/C1/ -f /mnt/Timina/lmorales/Public/Ustilago/C1/ID.csv -c USMA_521_v2_9 -t ExtGenesCov -M 2 -b /mnt/Timina/lmorales/Public/Ustilago/reference/annotation/GenesByChr/ -M 8
 ###############################################################################################################################################
 ## Libraries
 import argparse
@@ -29,23 +29,21 @@ ag = argparse.ArgumentParser(
     usage = "python3 Practice_script.py -f ~/USMA_ExpEvo_Samples.csv")
 ag.add_argument("-f", "--file", default = "", help = "csv with information of ID") # to read a csv file with sample imformation
 ag.add_argument("-n", "--email", default = "jcuamatzi@liigh.unam.mx", help = "If you want to receive a notification when the process is done")
-ag.add_argument("-g", "--gff", default = "", help = "path for the gff ubication")
+ag.add_argument("-c", "--chromosome", default = "", help = "name of the chromosome (USMA_521_v2_n)")
+ag.add_argument("-t", "--task", default = "", help = "Task of this script")
 ag.add_argument("-d", "--directory", default = "/home/jcuamatzi", help = "path to the project directory")
 ag.add_argument("-M", "--memory", default = "2", help = "RAM memory")
+ag.add_argument("-b", "--bedFile", default = "", help = "path to the bed files with information of the genes coordinates")
 ##
-necesito:
-    1 = la ruta del csv de los cromosomas (-f)
-    2 = el gff
-    
 #
 args = vars(ag.parse_args())
 arg_file = args["file"]
-reference_path = str(args["ref"])
 email = str(args["email"])
-directory = str(args["directory"])
+chrm = str(args["chromosome"])
 task = str(args["task"])
-window_kb = str(args["window"])
+directory = str(args["directory"])
 memory = str(args["memory"]) #RAM Memory
+bed_path = str(args["bedFile"])
 ##### Functions #####
 # Function to open a csv file
 # With this function, we are only open the file
@@ -80,10 +78,10 @@ def extcol (array, header):
     return arr
 # Function to write the header of sge file
 #
-#def header(chrs_ID, sge):
-def header(chrs_ID,sge):
-    path_error = wd_project + "log/error/"
-    path_out = wd_project + "log/out/"
+#def header(GeneByChr, sge):
+def header(smpls_ID, sge):
+    path_error = wd_project + "log/" + task + "/error/"
+    path_out = wd_project + "log/" + task + "/out/"
     if not os.path.exists(path_error):
         os.makedirs(path_error)
     if not os.path.exists(path_out):
@@ -93,12 +91,12 @@ def header(chrs_ID,sge):
 #$ -cwd
 . /etc/profile.d/modules.sh
 ##Error file
-#$ -e''', path_error + ID + ".error",'''
+#$ -e''', path_error + ID + "_" + task + ".error",'''
 ##Out file
-#$ -o''', path_out + ID + ".out",'''
+#$ -o''', path_out + ID + "_" + task + ".out",'''
 #$ -S /bin/bash
 ## Job's name
-#$ -N''', chr_ID,'''
+#$ -N''', task + "_" + smpls_ID,'''
 #$ -l vf='''+ memory +'''G
 #$ -pe openmp 10
 #$ -m e
@@ -106,62 +104,49 @@ source /etc/bashrc
 ## notification
 #$ -M ''' + email + '''
 ##''', file = sge)
+    print ("module load bedtools/2.27.1", file = sge)
+    print ("##", file = sge)
 
-def mapping (chrs_ID):
+def mapping (smpls_ID):
     save_sge = wd_project + "bin/SGE/" + task + "/"
     if not os.path.exists(save_sge):    
         os.makedirs(save_sge)
-    sge_name = save_sge + "/" + fecha + "_" + chrs_ID + "_" + sample_name + ".sge"
+    sge_name = save_sge + "/" + fecha + "_" + smpls_ID + "_" + chrm + "_CovByGeneAllPos.sge"
     sge = open(sge_name, "w")
-    header(chrs_ID,sge)
-    #
-    ref_genome = reference_path + "USMA_521_v2.fa"
+    header(smpls_ID, sge)
+    # paths
     bam_path = wd_project + "data/bam/mrkdup_addgp_bam/"
-    bam_file = bam_path + chrs_ID + "_" + sample_name + "_BWA.mrkdup.addgp.bam"
-    cnv_path = wd_project + "data/CNV/"
-    if not os.path.exists(cnv_path):
-        os.makedirs(cnv_path)
-    cnv_out_path = wd_project + "analysis/CNV/"
-    if not os.path.exists(cnv_out_path):
-        os.makedirs(cnv_out_path)
-    cnv_file = cnv_out_path + chrs_ID + "_" + sample_name + "_CNV." + window_kb + ".cnvnator"
-    #
+    # files
+    covbygene_path = wd_project + "analysis/coverage/ByChrmByGene/" + chrm + "/"
+    if not os.path.exists(covbygene_path):
+        os.makedirs(covbygene_path)
+    bam_file = bam_path + smpls_ID + "_" + sample_name + "_BWA.mrkdup.addgp.bam"
+    chr_bed_file = chrm + "_GFF.bed"
+    covbygene_file = smpls_ID + "_" + chrm + "_CovByGeneAllPos.tsv"   
     print ('''start=$(date +%s.%N)''', file = sge)
-    print ("## Start: Copy Number Variation ", file = sge )
-    print("cnvnator -root " + cnv_path + chrs_ID + "_" + sample_name + "_CNV.root -genome " + ref_genome + " -tree " + bam_file, file = sge) # 1
+    print ("## START: extracting the coverage by each gene in the chromosome " + chrm, file = sge)
+    print ("coverageBed -a " + bed_path + chr_bed_file + " -d -b " + bam_file + " > " + covbygene_path + covbygene_file, file = sge)
     print ("#", file = sge)
-    print("cnvnator -root " + cnv_path + chrs_ID + "_" + sample_name + "_CNV.root -his " + window_kb + " -d " + reference_path, file = sge) # 2
-    print ("#", file = sge)
-    print("cnvnator -root " + cnv_path + chrs_ID + "_" + sample_name + "_CNV.root -stat " + window_kb, file = sge) # 3
-    print ("#", file = sge)
-    print("cnvnator -root " + cnv_path + chrs_ID + "_" + sample_name + "_CNV.root -partition " + window_kb, file = sge) # 4
-    print ("#", file = sge)
-    print("cnvnator -root " + cnv_path + chrs_ID + "_" + sample_name + "_CNV.root -call " + window_kb + " > " + cnv_file, file = sge) # 5
-    print ("#", file = sge)
-    print ("#", file = sge)
-    print ("#", file = sge)
-    print ("## END COVERAGE", file = sge)
+    print ("## END: extracting the coverage by each gene in the chromosome " + chrm, file = sge)
     print ('''duration=$(echo "$(date +%s.%N) - $start" | bc)''', file = sge)
     print ('''execution_time=`printf "%.2f seconds" $duration`''', file = sge)
     print ('''echo "Script Execution Time: $execution_time"''', file = sge)
     sge.close()
     return sge_name  
 
-cat USMA_521_v2_allChr.gff | grep "USMA_521_v2_9" | sed '1d' | grep -n "gene" | grep -v "mRNA" | awk -F":" '{print $2 "\t"$3}'  | awk -F"\t" '{print $1"\t" $3"\t"$4"\t"$5"\t" $10}' | awk -F";" '{print $1 "\t" $3}' | awk -F"=" '{print $1 "\t" $2}' | awk -F"\t" '{print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$7}' > USMA_521_v2_9_Genes.txt
-
 ##### MAIN #####
 ## OPEN FILE ##
 matrix_csv = opencsv(arg_file)
 wd_project = directory
 ## IDs & NAMES ##
-chr_ID = extcol(matrix_csv, "ChrNum")
+smpls_ID = extcol(matrix_csv, "#ID")
 pyout_name = wd_project + "/log/Py_scripts/" + task + "/" + fecha + "_" + task + "_py.out"
 if not os.path.exists(wd_project + "/log/Py_scripts/" + task + "/"):
             os.makedirs(wd_project + "/log/Py_scripts/" + task + "/")
 pyout_name = os.path.normpath(pyout_name)
 pyoutput = open(pyout_name, "a+")
-for i in range(0, len(chrs_ID)):
-    ID = chrs_ID[i]
+for i in range(0, len(smpls_ID)):
+    ID = smpls_ID[i]
     sample_name = extcol(matrix_csv, "Name")[i]
     sge = mapping(ID)
     print(tiempo, file = pyoutput)
